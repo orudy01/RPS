694f6cc5b3d82291d8fb5650d7146e16191b01df
orudy01 <orudy01@icloud.com>
2022-06-14 12:07:44 -0700

    Connected the .js with HTML elemnts, game works
    Still to do:
        - Add hover elements to images/choices.
        - Add green or red glow to choices depending on outcome of round.
        - Add a start and game over splash screen that will restart the
        game when clicked on.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # RPS
-Rock/paper/scissors game made with Java Script
+Rock/paper/scissors game made with HTML, CSS, and Java Script
--- javascript.js
+++ javascript.js
@@ -1,36 +1,56 @@
+let userScore = 0;
+let computerScore = 0;
+const desciption_p = document.getElementById("description");
+const playerScore_div = document.getElementById("player-score");
+const computerScore_div = document.getElementById("computer-score")
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissor_div = document.getElementById("scissors");
+
+
+
 function computerPlay() {
     let answer = Math.floor(Math.random() * 3);
 
     if (answer === 0) {
-        answer = "Rock";
+        answer = "rock";
     }
     else if (answer === 1) {
-        answer = "Paper"
+        answer = "paper"
     }
     else if (answer === 2) {
-        answer = "Scissors"
+        answer = "scissors"
     }
 
     return answer;
 }
 
 function playRound(playerSelection, computerSelection) {
-
     let finalMsg = undefined;
+
+    if (userScore === 5 || computerScore === 5) {
+        return;
+    }
 
     let pSelection = playerSelection.toLowerCase();
     let cSelection = computerSelection.toLowerCase();
 
     if (pSelection === cSelection) {
-        finalMsg = "Draw, play again"
+        finalMsg = `Tie round! You and the computer picked ${playerSelection}.`;
     }
-
     else if (
         (pSelection === "rock" && cSelection === "scissors") ||
         (pSelection === "paper" && cSelection === "rock") ||
         (pSelection === "scissors" && cSelection === "paper")
     ) {
-        finalMsg = `You won! You picked ${playerSelection} while the computer picked ${computerSelection}.`;
+        userScore++;
+        playerScore_div.textContent = userScore.toString();
+        if (userScore === 5) {
+            finalMsg = "YOU WIN!";
+        }
+        else {
+            finalMsg = `You won! You picked ${playerSelection} while the computer picked ${computerSelection}.`;
+        }
     }
 
     else if (
@@ -38,15 +58,36 @@ function playRound(playerSelection, computerSelection) {
         (cSelection === "paper" && pSelection === "rock") ||
         (cSelection === "scissors" && pSelection === "paper")
     ) {
-        finalMsg = `You lost! You picked ${playerSelection} while the computer picked ${computerSelection}.`;
+        computerScore++;
+        computerScore_div.textContent = computerScore.toString();
+        if (computerScore === 5) {
+            finalMsg = "YOU LOSE!";
+        }
+        else {
+            finalMsg = `You lost! You picked ${playerSelection} while the computer picked ${computerSelection}.`;
+        }
     }
 
-    return finalMsg
+    desciption_p.textContent = finalMsg;
+
 }
 
-/*
-for (let i = 0; i < 5; i++) {
-    let choice = prompt("Whats your weapon of choice?");
-    console.log(playRound(choice, computerPlay()));
+
+
+function main() {
+
+    rock_div.addEventListener('click', function () {
+        playRound("rock", computerPlay());
+    })
+
+    paper_div.addEventListener('click', function () {
+        playRound("paper", computerPlay());
+    })
+
+    scissor_div.addEventListener('click', function () {
+        playRound("scissors", computerPlay());
+    })
+
 }
-*/
+
+main();
